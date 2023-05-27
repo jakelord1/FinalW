9a116b891fec6994af5138c4ffa7a70e2ce2b0e5
jakelord1 <jakelord1.vo@gmail.com>
2023-05-27 21:07:06 +0300

    added sort

--- FinalWork/FinalWork.cpp
+++ FinalWork/FinalWork.cpp
@@ -3,9 +3,10 @@
 #define CO cout << 
 #define T Task.
 #define SI Size
-#define NSI NSize
+#define SS NSize
 #define E std::endl
 #define CL system("cls");
+#define SH S CO "id" << '\t' << "Prior" << '\t' << "Date" << "\t\t" << "Time" << '\t' << "Name" << E;
 
 struct Time {
     int min;
@@ -17,11 +18,13 @@ struct Time {
 const size_t Size = 250;
 const size_t NSize = 50;
 struct Task {
-    char name[NSI];
-    char descript[SI];
+    char name[SS];
     int prior;
+    char descript[SI];
     Time time;
 };
+
+
 
 void TaskShow(Task task) {
     S CO "Task:\nName: " << task.name;
@@ -29,17 +32,23 @@ void TaskShow(Task task) {
     S CO "\nPriority: " << task.prior;
     S CO "\nDate: " << task.time.day << '\\' << task.time.month << '\\' << task.time.year << "\t" << task.time.hour << ':' << task.time.min;
 }
+
+
+
+
+
 Task TaskEdit(Task task) {
     CL
     S CO "Task Editing" << E;
+    S cin.getline(task.name, SS);
     S CO "Enter name of task(до 50 символов): ";
-    S cin.getline(task.name, NSI);
-    _sleep(2000);
-    S CO "Enter task description(до 250 символов): ";
-    S cin.getline(task.descript, SI);
-
+    S cin.getline(task.name, SS);
+    
     S CO "Enter priority of task(1 - 4): ";
     S cin >> task.prior;
+    S cin.getline(task.descript, SI);
+    S CO "Enter task description(до 250 символов): ";
+    S cin.getline(task.descript, SI);
 
     S CO "Enter date of task:" << E;
     S CO  "Year: ";
@@ -54,6 +63,10 @@ Task TaskEdit(Task task) {
     S cin >> task.time.min;
     return task;
 }
+
+
+
+
 Task TaskAdder(Task task) {
     bool choise = 1;
     Task z = task;
@@ -63,24 +76,36 @@ Task TaskAdder(Task task) {
     S cin >> choise;
     CL
     if (choise) {
-        S CO "Task added";
+        S CO "Task added" <<E;
         return task;
     }
     else {
         return z;
     }
 }
+
+
+
+
 Task* TaskDeleter(Task tasks[], int choosed) {
-    for (size_t i = 0; i < NSI; i++)
+    for (size_t i = 0; i < SS; i++)
     {
         if (i >= choosed) {
             tasks[i] = tasks[i + 1];
         }
         
     }
-    S CO "Task deleted";
+    CL
+    S CO "Task deleted" << E;
     return tasks;
 }
+
+
+
+
+
+
+
 void TasksShower(Task task, int i) {
     S CO i << "\t" << task.prior << '\t' << task.time.day << '\\' << task.time.month << '\\' << task.time.year << "\t" << task.time.hour << ':' << task.time.min << '\t' << task.name << E;
 }
@@ -102,6 +127,12 @@ Task TaskEditor(Task task) {
         return z;
     }
 }
+
+
+
+
+
+
 Task* TaskSelected(Task tasks[], int i) {
     CL
     TaskShow(tasks[i]);
@@ -125,6 +156,35 @@ Task* TaskSelected(Task tasks[], int i) {
         break;
     }
 }
+
+
+
+
+
+
+
+void quickSortP(Task* tasks, int N) {
+    long i = 0, j = N - 1;
+    Task temp, p;
+    p = tasks[N >> 1];
+    do {
+        while (tasks[i].prior < p.prior) i++;
+        while (tasks[j].prior > p.prior) j--;
+
+        if (i <= j) {
+            temp = tasks[i]; tasks[i] = tasks[j]; tasks[i] = temp;
+            i++; j--;
+        }
+    } while (i <= j);
+
+    if (j > 0) quickSortP(tasks, j);
+    if (N > i) quickSortP(tasks + i, N - i);
+
+
+}
+
+
+
 void TaskSearch(Task tasks[]) {
     S CO "Search by:\n1 - name\t2 - description\t3 - priority\t4 - date" << E;
     int choose;
@@ -132,9 +192,10 @@ void TaskSearch(Task tasks[]) {
     switch (choose)
     {
     case 1: {
+        SH
         char search[5], comp[5];
         S cin >> search;
-        for (size_t i = 0; i < NSI; i++)
+        for (size_t i = 0; i < SS; i++)
         {
             for (size_t j = 0; j < 5; j++)
             {
@@ -144,6 +205,7 @@ void TaskSearch(Task tasks[]) {
                 TasksShower(tasks[i], i);
             }
         }
+        S CO "Choose task: ";
         S cin >> choose;
         TaskSelected(tasks, choose);
         break;
@@ -151,7 +213,8 @@ void TaskSearch(Task tasks[]) {
     case 2: {
         char search[5], comp[5];
         S cin >> search;
-        for (size_t i = 0; i < NSI; i++)
+        SH
+        for (size_t i = 0; i < SS; i++)
         {
             for (size_t j = 0; j < 5; j++)
             {
@@ -161,19 +224,23 @@ void TaskSearch(Task tasks[]) {
                 TasksShower(tasks[i], i);
             }
         }
+        S CO "Choose task: ";
         S cin >> choose;
         TaskSelected(tasks, choose);
         break;
     }
     case 3: {
         int cmp;
+        S CO E << "Enter proirity: ";
         S cin >> cmp;
-        for (size_t i = 0; i < NSI; i++)
+        SH
+        for (size_t i = 0; i < SS; i++)
         {
             if (cmp == tasks[i].prior) {
                 TasksShower(tasks[i], i);
             }
         }
+        S CO "Choose task: ";
         S cin >> choose;
         TaskSelected(tasks, choose);
         break;
@@ -191,47 +258,30 @@ void TaskSearch(Task tasks[]) {
         S cin >> date[3];
         S CO  "Minutes: ";
         S cin >> date[4];
-        for (size_t i = 0; i < NSI; i++)
+        SH
+        for (size_t i = 0; i < SS; i++)
         {
             if (date[0] == tasks[i].time.year && date[1] == tasks[i].time.month && date[2] == tasks[i].time.day && date[3] == tasks[i].time.hour && date[4] == tasks[i].time.min) {
                 TasksShower(tasks[i], i);
             }
         }
+        S CO "Choose task: ";
         S cin >> choose;
         TaskSelected(tasks, choose);
     }
     default:
         break;
     }
-}
-
-void quickSortR(Task* tasks, int N) {
-    long i = 0, j = N - 1;
-    Task temp, p;
-    p = tasks[N >> 1];
-    do {
-        while (tasks[i].prior < p.prior) i++;
-        while (tasks[j].prior > p.prior) j--;
-
-        if (i <= j) {
-            temp = tasks[i]; tasks[i] = tasks[j]; tasks[i] = temp;
-            i++; j--;
-        }
-    } while (i <= j);
-
-    if (j > 0) quickSortR(tasks, j);
-    if (N > i) quickSortR(tasks + i, N - i);
-
 
 }
 
 
-void TaskSort() {
 
-}
+
+
 void TaskMenu(Task tasks[], Time current) {
-    S CO "Enter action:\n1 - Choose task\t2 - Show tasks by time\t3 - Sort tasks\t0 - Exit programm" << E;
-    int choise;
+    S CO "Enter action:\n1 - Choose task\t2 - Show tasks by time\t3 - Search tasks\t0 - Exit programm" << E;
+    int choise, sel;
     S cin >> choise;
     switch (choise)
     {
@@ -248,32 +298,84 @@ void TaskMenu(Task tasks[], Time current) {
         switch (choise)
         {
         case 1: {
-            for (size_t i = 0; i < NSI; i++)
+            SH
+            for (size_t i = 0; i < SS; i++)
             {
                 if (current.year == tasks[i].time.year && current.month == tasks[i].time.month && current.day == tasks[i].time.day) {
+                    
                     TasksShower(tasks[i], i);
                 }
             }
-            TaskMenu(tasks, current);
+            sel = 1;
+            S CO "Choose action\n1 - Choose task\t2 - Sort list by priority" << E;
+            S cin >> choise;
+            switch (choise)
+            {
+            case 1: {
+                S CO "Choose task: ";
+                S cin >> choise;
+                TaskSelected(tasks, choise);
+                break;
+            }
+            case 2: {
+                quickSortP(tasks, SS);
+                if (sel == 1) {
+                    for (size_t i = 0; i < SS; i++)
+                    {
+                        if (current.year == tasks[i].time.year && current.month == tasks[i].time.month && current.day == tasks[i].time.day) {
+
+                            TasksShower(tasks[i], i);
+                        }
+                    }
+                }
+                else if (sel == 2) {
+                    for (size_t i = 0; i < SS; i++)
+                    {
+                        if (current.year == tasks[i].time.year && current.month == tasks[i].time.month && tasks[i].time.day - current.day <= 7) {
+
+                            TasksShower(tasks[i], i);
+                        }
+                    }
+                }
+                else if (sel == 3) {
+                    for (size_t i = 0; i < SS; i++)
+                    {
+                        if (current.year == tasks[i].time.year && current.month == tasks[i].time.month) {
+
+                            TasksShower(tasks[i], i);
+                        }
+                    }
+                }
+                break;
+            }
+            default:
+                break;
+            }
             break;
         }
         case 2: {
-            for (size_t i = 0; i < NSI; i++)
+            SH
+            for (size_t i = 0; i < SS; i++)
             {
                 if (current.year == tasks[i].time.year && current.month == tasks[i].time.month && tasks[i].time.day - current.day <= 7) {
+                    
                     TasksShower(tasks[i], i);
                 }
             }
+            sel = 2;
             TaskMenu(tasks, current);
             break;
         }
         case 3: {
-            for (size_t i = 0; i < NSI; i++)
+            SH
+            for (size_t i = 0; i < SS; i++)
             {
                 if (current.year == tasks[i].time.year && current.month == tasks[i].time.month) {
+                    
                     TasksShower(tasks[i], i);
                 }
             }
+            sel = 3;
             TaskMenu(tasks, current);
             break;
         }
@@ -289,10 +391,15 @@ void TaskMenu(Task tasks[], Time current) {
         break;
     }
 }
+
+
+
+
+
 int main()
 {
     setlocale(LC_ALL, "");
-    Task* tasks = new Task[NSI];
+    Task* tasks = new Task[SS];
     Time current;
     S CO "Enter curent date:" << E;
     S CO  "Year: ";
@@ -306,7 +413,7 @@ int main()
     S CO  "Minutes: ";
     S cin >> current.min;
 
-    for (size_t i = 0; i < NSI; i++)
+    for (size_t i = 0; i < SS; i++)
     {
         if (tasks[i].prior < 0) {
             tasks[i].prior = 0;
@@ -318,6 +425,7 @@ int main()
         }
     }
     while (1) {
+        S CO "id" << '\t' << "Prior" << '\t' << "Date" << '\t' << "Time" << '\t' << "Name" << E;
         for (size_t i = 0; i < 5; i++)
         {
             TasksShower(tasks[i], i);
